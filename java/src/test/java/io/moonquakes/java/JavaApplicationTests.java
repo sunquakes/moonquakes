d85bede915cd9083edab761c8448d631700a6118
Robert <sunquakes@outlook.com>
2022-08-25 20:46:57 +0800

    Project init

---
+++ java/src/test/java/io/moonquakes/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.moonquakes.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
