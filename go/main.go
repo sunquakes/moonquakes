7269e319c3ca214353f209b4774deb3b57bbab62
Robert <sunquakes@outlook.com>
2023-02-18 18:46:45 +0800

    Upgrade.

--- go/main.go
+++ go/main.go
@@ -10,41 +10,41 @@ import (
 
 func main() {
 	go func() {
-		s, _ := jsonrpc4go.NewServer("tcp", "127.0.0.1", "3601")
+		s, _ := jsonrpc4go.NewServer("tcp", 3601)
 		s.Register(new(GoTcp))
 		s.Start()
 	}()
 	go func() {
-		s, _ := jsonrpc4go.NewServer("http", "127.0.0.1", "3602")
+		s, _ := jsonrpc4go.NewServer("http", 3602)
 		s.Register(new(GoHttp))
 		s.Start()
 	}()
 	tk := task.NewTask("tk", "*/5 * * * * *", func(ctx context.Context) error {
 		result := new(Result)
-		phpTcpClient, _ := jsonrpc4go.NewClient("tcp", "127.0.0.1", "9503")
-		phpHttpClient, _ := jsonrpc4go.NewClient("http", "127.0.0.1", "9504")
+		phpTcpClient, _ := jsonrpc4go.NewClient("php_tcp", "tcp", "127.0.0.1:9503")
+		phpHttpClient, _ := jsonrpc4go.NewClient("php_http", "http", "127.0.0.1:9504")
 
-		javaTcpClient, _ := jsonrpc4go.NewClient("tcp", "127.0.0.1", "3201")
-		javaHttpClient, _ := jsonrpc4go.NewClient("http", "127.0.0.1", "3202")
+		javaTcpClient, _ := jsonrpc4go.NewClient("java_tcp", "tcp", "127.0.0.1:3201")
+		javaHttpClient, _ := jsonrpc4go.NewClient("java_http", "http", "127.0.0.1:3202")
 
 		a := rand.Intn(100)
 		b := rand.Intn(100)
-		_ = phpTcpClient.Call("php_tcp/add", Params{Args{a, b}}, result, false)
+		_ = phpTcpClient.Call("add", Params{Args{a, b}}, result, false)
 		fmt.Printf("[tcp] Go asked:\"%d+%d=?\"; PHP answered:\"%d\"\n", a, b, (*result).C)
 
 		a = rand.Intn(100)
 		b = rand.Intn(100)
-		_ = phpHttpClient.Call("php_http/add", Params{Args{a, b}}, result, false)
+		_ = phpHttpClient.Call("add", Params{Args{a, b}}, result, false)
 		fmt.Printf("[http] Go asked:\"%d+%d=?\"; PHP answered:\"%d\"\n", a, b, (*result).C)
 
 		a = rand.Intn(100)
 		b = rand.Intn(100)
-		_ = javaTcpClient.Call("java_tcp/add", Params{Args{a, b}}, result, false)
+		_ = javaTcpClient.Call("add", Params{Args{a, b}}, result, false)
 		fmt.Printf("[tcp] Go asked:\"%d+%d=?\"; Java answered:\"%d\"\n", a, b, (*result).C)
 
 		a = rand.Intn(100)
 		b = rand.Intn(100)
-		_ = javaHttpClient.Call("java_http/add", Params{Args{a, b}}, result, false)
+		_ = javaHttpClient.Call("add", Params{Args{a, b}}, result, false)
 		fmt.Printf("[http] Go asked:\"%d+%d=?\"; Java answered:\"%d\"\n", a, b, (*result).C)
 		return nil
 	})
