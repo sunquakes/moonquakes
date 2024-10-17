4c75f479888b1bfbe4dfc767980bce0ee4e2f596
Shing Rui <sunquakes@outlook.com>
2024-10-17 22:01:09 +0800

    Upgrade jsonrpc4j to 3.0.0-SNAPSHOT.

--- java-http/src/main/java/io/moonquakes/javahttp/client/IPhpHttpClient.java
+++ java-http/src/main/java/io/moonquakes/javahttp/client/IPhpHttpClient.java
@@ -5,7 +5,7 @@ import com.sunquakes.jsonrpc4j.JsonRpcProtocol;
 import io.moonquakes.javahttp.dto.ArgsDto;
 import io.moonquakes.javahttp.dto.ResultDto;
 
-@JsonRpcClient(value = "php_http", protocol = JsonRpcProtocol.http)
+@JsonRpcClient(value = "php_http", protocol = JsonRpcProtocol.HTTP)
 public interface IPhpHttpClient {
     ResultDto add(ArgsDto args);
 }
--- java-tcp/src/main/java/io/moonquakes/javatcp/client/IPhpTcpClient.java
+++ java-tcp/src/main/java/io/moonquakes/javatcp/client/IPhpTcpClient.java
@@ -5,7 +5,7 @@ import com.sunquakes.jsonrpc4j.JsonRpcProtocol;
 import io.moonquakes.javatcp.dto.ArgsDto;
 import io.moonquakes.javatcp.dto.ResultDto;
 
-@JsonRpcClient(value = "php_tcp", protocol = JsonRpcProtocol.tcp)
+@JsonRpcClient(value = "php_tcp", protocol = JsonRpcProtocol.TCP)
 public interface IPhpTcpClient {
     ResultDto add(ArgsDto args);
 }
